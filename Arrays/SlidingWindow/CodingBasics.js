2875a258eeba282847584959c751f818b6ad13a2
9802HEMENSAN <hmahilange9802@gmail.com>
2023-05-18 00:05:38 +0530

    Add One to Array

---
+++ Arrays/SlidingWindow/CodingBasics.js
@@ -0,0 +1,38 @@
+ 
+// let num=97;
+// CheckGrade(num)
+// function CheckGrade(num){
+    
+//     if(num>=90 && num <=100){
+//         console.log("A");
+//     }
+//     else if(num>=80 && num < 90){
+//         console.log("B");
+//     }
+//     if(num>=70 && num < 80){
+//         console.log("C");
+//     }
+//     if(num>=60 && num < 70){
+//         console.log("D");
+//     }
+//     if( num <= 60 ){
+//         console.log("F");
+//     }
+// }
+ 
+// let num=22;
+// IsEligible(num)
+//  function IsEligible(num){
+     
+//     //  if(num>=18){
+//     //   console.log("Eligible for Voting" )
+//     //  }else{
+//     //   console.log("Not Eligible for voting")
+//     //  }
+     
+//      num>=18 ?  console.log("Eligible for Voting" ) :console.log("Not Eligible for voting")
+//  }
+
+// console.log(1 ==1 )
+
+ 
